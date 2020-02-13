5159c6cfc45fecc83cdbbc80c23fcea38d0380c1
Rajiv <1998irajeev@gmail.com>
2020-02-14 00:27:15 +0530

    static path changed

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const app = express()
 const https = require('https');
 const path = require('path')
 
-app.use(express.static("client/build"));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get("/api", (req, res) => {
     const data = [
@@ -19,8 +16,14 @@ app.get("/api", (req, res) => {
 })
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/client/build/static/index.html");
+  res.sendFile;
 })
+
+if(process.env.NODE_ENV === 'production') {  
+  app.use(express.static(path.join(__dirname, 'client/build'))); 
+  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })
+}
+
 
 const port = process.env.PORT || 5000
 
